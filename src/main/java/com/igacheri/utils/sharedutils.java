e4d3556672242f784f297c0f21d1809839c262e4
idah <idah@moringaproject.com>
2023-07-31 19:04:23 +0300

    added the shared utility class

--- src/main/java/com/igacheri/utils/sharedutils.java
+++ src/main/java/com/igacheri/utils/sharedutils.java
@@ -1,4 +1,10 @@
 package com.igacheri.utils;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.Map;
 public class sharedutils {
+    public static String render(Map<Integer, Object> model, String templateName) {
+        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templateName));
+    }
 }
